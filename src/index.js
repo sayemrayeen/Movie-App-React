3c4fb0e98b7572427ccf59f3530a6c68460a564d
Sayem <sayemrayeen068@gmail.com>
2022-12-17 00:39:14 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <Router>
+      <Router basename={process.env.REACT_APP_MOVIE_KEY}>
         <App />
       </Router>
     </AppProvider>
